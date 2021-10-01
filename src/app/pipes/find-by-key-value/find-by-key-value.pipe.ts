187cf25e978d69f3b6720308423a2d25f29fcb83
Rikesh <recasechhunju@gmail.com>
2021-10-01 14:16:28 +0545

    Update pipe to return the string value

--- src/app/pipes/find-by-key-value/find-by-key-value.pipe.ts
+++ src/app/pipes/find-by-key-value/find-by-key-value.pipe.ts
@@ -4,9 +4,11 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'findByKeyValue'
 })
 export class FindByKeyValuePipe implements PipeTransform {
-
-  transform(value: Array<{[key: string]: any}>, key: string, cmpValue: string): any {
-    return value.find(val => val[key] === cmpValue);
+  transform(value: Array<{ [key: string]: any }>, key: string, cmpValue: string): string {
+    const element = value.find((val) => val[key] === cmpValue);
+    if (element) {
+      return element.name;
+    }
+    return '';
   }
-
 }
