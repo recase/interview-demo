fcf1526ddfb4306a5e1239f7c9b65e7f67f4bc48
Rikesh <recasechhunju@gmail.com>
2021-10-01 13:17:40 +0545

    Add loading component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { TableComponent } from './components/table/table.component';
 import { DetailPanelComponent } from './components/detail-panel/detail-panel.component';
 import { MainService } from './services/main.service';
 import { FindByKeyValuePipe } from './pipes/find-by-key-value/find-by-key-value.pipe';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent,
     DetailPanelComponent,
-    FindByKeyValuePipe
+    FindByKeyValuePipe,
+    LoadingComponent
   ],
   imports: [
     BrowserModule
